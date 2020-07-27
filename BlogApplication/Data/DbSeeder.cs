8fc200561e166f0d7fc5ad49742ce5c5755b5e18
Stefan Gherman <scgheman@gmail.com>
2020-07-27 08:39:32 +0300

    Add dbseeder class

--- BlogApplication/Data/DbSeeder.cs
+++ BlogApplication/Data/DbSeeder.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BlogApplication.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,34 @@ namespace BlogApplication.Data
 {
     public class DbSeeder
     {
+        public static void Init(BlogContext context)
+        {
+            if (context.Post.Any())
+            {
+                return;
+            }
 
+            var users = new User[]
+            {
+                new User{ FirstName="Tony", LastName="Gonzalez"},
+                new User{ FirstName="Albert", LastName="Thompson"},
+                new User{ FirstName="Frank", LastName="Nunez"},
+                new User{ FirstName="Karen", LastName="Powell"},
+            };
+            context.User.AddRange(users);
+            context.SaveChanges();
+
+
+            var Posts = new Post[]
+            {
+                new Post {Content = "Learn how to change a tire in just 45 seconds!", UserId = 1},
+                new Post {Content = "Hunting foxes has never been easier", UserId = 1},
+                new Post {Content = "Clubbing is for losers", UserId = 2},
+                new Post {Content = "Vamos todos a la plaza!", UserId = 3},
+            };
+
+            context.Post.AddRange(Posts);
+            context.SaveChanges();
+        }
     }
 }
