c44149f14cd4addcb16907c124b03a6b740e0a41
Stefan Gherman <scgheman@gmail.com>
2020-07-28 04:27:32 +0300

    Add sample js file for fetch

---
+++ BlogApplication/wwwroot/js/blogPage.js
@@ -0,0 +1,11 @@
+﻿
+const btn = document.getElementById("sortByDate");
+
+
+$(document).ready(() => {
+    const $sortButton = $("#sortByDate");
+    $sortButton.css("margin-bottom", "5%");
+    $sortButton.on("click", () => {
+       
+    });
+});
